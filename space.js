7d05e6631d1cc55a1eb58f82b0ce880227115019
TriedSalt27459 <mikeeniola@hotmail.com>
2024-05-23 20:23:14 -0600

    Update space.js

--- space.js
+++ space.js
@@ -56,13 +56,13 @@ window.onload = function() {
 
     //load images
     shipImg = new Image();
-    shipImg.src = "./SSBKK20_Goku.png";
+    shipImg.src = "SSBKK20_Goku.png";
     shipImg.onload = function() {
         context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
     }
 
     alienImg = new Image();
-    alienImg.src = "./Fused_Zamasu.png";
+    alienImg.src = "Fused_Zamasu.png";
     createAliens();
 
     requestAnimationFrame(update);
